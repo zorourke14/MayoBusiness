00993e31449509b473e491745750bcf26efebec4
zorourke14 <zorourke14@gmail.com>
2024-11-07 15:47:29 -0500

    First commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { withExpoSnack } from 'nativewind';
+
+
+export default function App() {
+  return (
+    <View className = "flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Recipe</Text>
+      <StatusBar style="auto" />
+      <Link href = "/home" style = {{color:'blue'}}>Go to profile</Link>
+    </View>
+  );
+}
+
