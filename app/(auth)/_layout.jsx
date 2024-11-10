5ecdf6072de9ea8e972ff873c7c5e4a4812ca3ad
zorourke14 <zorourke14@gmail.com>
2024-11-09 22:31:04 -0500

    Started sign up page

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { Stack } from 'expo-router';
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name = "sign-in"
+        options={{
+          headerShown: false
+        }}
+        />
+
+        <Stack.Screen
+          name = "sign-up"
+          options={{
+          headerShown: false
+        }}
+        />
+      </Stack>
+
+      <StatusBar
+        backgroundColor='#161622'
+        style='light'
+      />
+    </>
   )
 }
 
